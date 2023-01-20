6da1f3e7e7b5cb4e4914c4de497334f33dcec637
komalsraut <122728472+komalsraut@users.noreply.github.com>
2023-01-20 18:07:32 +0530

    Update BaseTest.java

--- src/main/java/Base/BaseTest.java
+++ src/main/java/Base/BaseTest.java
@@ -3,7 +3,11 @@ package Base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-
+webDriverManager.chromeDriver().setup();
+	webdriver driver=new chromedriver();
+	driver.manage().window().maximize();
+	driver.get("url");
+	
 	
 	
 }
